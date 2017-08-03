744361da7c7c39dc1e369ffd9c2303c8a9f72b7f
Szymon Rozkocha <szymon@rozkocha.pl>
2017-08-03 18:27:33 +0200

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+    <a [routerLink]="['/articles']">Articles</a>
+    <a [routerLink]="['/galleries']">Galleries</a>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   
